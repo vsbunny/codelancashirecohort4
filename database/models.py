58e5213730ef7cf166ff79f0dbbc2187b5d253be
Vanny <vannyaspasova@gmail.com>
2023-06-11 22:19:17 +0100

    final assignment edits

--- database/models.py
+++ database/models.py
@@ -6,20 +6,21 @@ from database.connection import Base #Because all SQLAlchemy models/classes will
 class Pokemon(Base):
     __tablename__ = "Pokemon" #__tablename__ attribute tells SQLAlchemy the name of the table to use in the database for each of these models
 
-    id = Column(Integer, primary_key=True, index=True) #each of the attributes represent columns
-    #drums = relationship("Drum", backref="owner")
-    classification = Column(String,  index=True)
-    name = Column(String,  index=True)
-    type1 = Column(String, index=True)
-    type2 = Column(String, index=True)
-    generation = Column(Integer, index=True)
-    
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True) #each of the attributes represent columns
+    name = Column(String(50))
+    classification = Column(String(150))
+    type1 = Column(String(50))
+    type2 = Column(String(50))
+    generation = Column(Integer)
+    #1 to 1 relationship with PokemonStats - uselist=False means only one object from PokemonStats is related to an object of Pokemon
+    stats = relationship("PokemonStats", backref="pokemon", uselist=False)
+
 class PokemonStats(Base):
     __tablename__ = "Pokemon_stats"
-
-    pokemon_id = Column(Integer, primary_key=True, index=True)
-    height_m = Column(Float, index=True)
-    weight_kg = Column(Float, index=True)
-    attack = Column(Integer, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    pokemon_id = Column(Integer, ForeignKey("Pokemon.id"))
+    height_m = Column(Float, nullable=True)
+    weight_kg = Column(Float, nullable=True)
+    attack = Column(Integer, nullable=True)
      
 
