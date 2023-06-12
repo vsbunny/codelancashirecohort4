adfb39f4e1c096aad4d2f46e5dc3dd5b72899560
Vanny <vannyaspasova@gmail.com>
2023-06-12 16:40:25 +0100

    fixed uploaded file code

--- helpers/filehandling.py
+++ helpers/filehandling.py
@@ -1,18 +1,18 @@
 import csv
 from database.models import Pokemon, PokemonStats
 
-#def uploadCSVFileToPokemonDatabase(file, db):
+def uploadPokemonData(file, db):
     # extract data from file
-    #fileContent = file.file.read().decode("utf-8")
+    fileContent = file.file.read().decode("utf-8")
     
     # convert data to rows using csv module. Its a built in module in python
-    #rows = csv.reader(fileContent.splitlines(), delimiter=",")
+    rows = csv.reader(fileContent.splitlines(), delimiter=",")
   
     # skip the first row as it contains the column names
-    #next(rows)
+    next(rows)
 
     # iterate over the rows and add them to the database
-    #for row in rows:
+    for row in rows:
         # uncomment the line below and see the output in terminal it should show you the data in each row (list of rows)
         # print(row)
 
@@ -22,29 +22,29 @@ from database.models import Pokemon, PokemonStats
         # row[0] means first row first column cell. We skipped the first row as it contains the column names
         # row[1] means first row second column cell so on and so forth
         # on next iteration of the loop row[0] will be second row first column cell and so on and so forth
-        #pokemon = Pokemon(classification=row[1], name=row[3], type1=row[5], type2=row[6], generation=row[7])
+        pokemon = Pokemon(classification=row[1], name=row[3], type1=row[5], type2=row[6], generation=row[7])
         # here we are adding the pokemon object to the database session and not to the database itself
-        #db.add(pokemon)
+        db.add(pokemon)
     
     # commit the changes to the database which will insert the data into the database 
-    #db.commit()
+    db.commit()
     # check the result in the database using pgadmin
 
-#def uploadCSVFileToPokemonStatsDatabase(file, db):
+def uploadPokemonStats(file, db):
     # the process is similar to the above function
-     # extract data from file
-    #fileContent = file.file.read().decode("utf-8")
+    # extract data from file
+    fileContent = file.file.read().decode("utf-8")
     
     # convert data to rows using csv module. Its a built in module in python
-    #rows = csv.reader(fileContent.splitlines(), delimiter=",")
+    rows = csv.reader(fileContent.splitlines(), delimiter=",")
   
     # skip the first row as it contains the column names
-    #next(rows)
+    next(rows)
 
      # iterate over the rows and add them to the database
-    #for row in rows:
+    for row in rows:
         # uncomment the line below and see the output in terminal it should show you the data in each row (list of rows)
-        # print(row)
+        print(row)
 
         # add the row to the database. Remember the sequence of the data in the row matters
         # we will create a bulk insert to avoid performance issue and also to avoid multiple commits
@@ -59,10 +59,10 @@ from database.models import Pokemon, PokemonStats
         # To migrate a new table with nullable columns, we need to drop the table and then create it again
         # To drop the table, we need to go to pgadmin and drop the table manually by right clicking on the table and selecting Delete/Drop
 
-        #pokemonStats = PokemonStats(pokemon_id=row[0], height_m=row[26] if row[26] else None, weight_kg=row[31] if row[31] else None, attack=row[19])
+        pokemonStats = PokemonStats(pokemon_id=row[0], height_m=row[26] if row[26] else None, weight_kg=row[31] if row[31] else None, attack=row[19])
         # here we are adding the pokemon object to the database session and not to the database itself
-        #db.add(pokemonStats)
+        db.add(pokemonStats)
     
     # commit the changes to the database which will insert the data into the database 
-    #db.commit()
+    db.commit()
     # check the result in the database using pgadmin
--- routes/pokemon_routes.py
+++ routes/pokemon_routes.py
@@ -5,14 +5,13 @@ from database.models import PokemonStats as PokemonStatsTable
 import csv
 from database.connection import get_db
 from sqlalchemy.orm import Session
-#import datasets somehow?
 
-
-router = APIRouter(prefix="/pokemons",tags=["PokeDex"]) 
+router = APIRouter(prefix="/pokemons",tags=["PokeDex App"]) 
 #router.get and router.put etc... 
 
-@router.post("/uploadCSV")
-def uploadCSVFileToPokemonDatabase(file: UploadFile = File(...), db: Session=Depends(get_db)):
+#Upload the PokemonData File First; UploadFile=File(...) db:Session=Depends(get_db) is the connection to PostgreSQL
+@router.post("/uploadPokemonData")
+def uploadPokemonData(file: UploadFile = File(...), db: Session=Depends(get_db)):
     # extract data from file
     fileContent = file.file.read().decode("utf-8")
     
@@ -39,11 +38,51 @@ def uploadCSVFileToPokemonDatabase(file: UploadFile = File(...), db: Session=Dep
     
     # commit the changes to the database which will insert the data into the database 
     db.commit()
-    return{"data":pokemon}
+    return{"message":"The file has been uploaded"}
     # check the result in the database using pgadmin
-#def trymodels(db:Session = Depends(get_db)):
-    #return{"status":"success"}
+
+#Upload PokemonStats to the database; logic, same as above
+@router.post("/uploadPokemonStats")
+def uploadPokemonStats(file: UploadFile=File(...), db: Session=Depends(get_db)):
+    # the process is similar to the above function
+    # extract data from file
+    fileContent2 = file.file.read().decode("utf-8")
+    
+    # convert data to rows using csv module. Its a built in module in python
+    rows = csv.reader(fileContent2.splitlines(), delimiter=",")
+  
+    # skip the first row as it contains the column names
+    next(rows)
+
+     # iterate over the rows and add them to the database
+    for row in rows:
+        # uncomment the line below and see the output in terminal it should show you the data in each row (list of rows)
+        print(row)
+
+        # add the row to the database. Remember the sequence of the data in the row matters
+        # we will create a bulk insert to avoid performance issue and also to avoid multiple commits
+        # on the first iteration of the loop
+        # row[0] means first row first column cell. We skipped the first row as it contains the column names
+        # row[1] means first row second column cell so on and so forth
+        # on next iteration of the loop row[0] will be second row first column cell and so on and so forth
+        # on some rows, there are missing values for height and weight. we need to make sure to handle these edge cases as well
+        # we are using the if condition to check if the value is present or not. If it is present then we are using it else we are using None
+        # To accepts None values, we need to make sure that the column in the database is nullable
+        # I have made the height_m and weight_kg columns nullable in the models.py and also changed their data type to Float
+        # To migrate a new table with nullable columns, we need to drop the table and then create it again
+        # To drop the table, we need to go to pgadmin and drop the table manually by right clicking on the table and selecting Delete/Drop
+
+        pokemonStats = PokemonStatsTable(pokemon_id=row[0], height_m=row[26] if row[26] else None, weight_kg=row[31] if row[31] else None, attack=row[19])
+        # here we are adding the pokemon object to the database session and not to the database itself
+        db.add(pokemonStats)
+    
+    # commit the changes to the database which will insert the data into the database 
+    db.commit()
+    return{"message":"The file has been uploaded!"}
+    # check the result in the database using pgadmin
+
 #to get all pokemons
+#Thinking the Pokemon Response_model should be a dictionary, so can see both pokemondata + pokemonStats
 #@router.get("/", response_model=list[DrumResponseModel])
 #def getDrums(db: Session = Depends(get_db)): #get the data from the database
     #drums = db.query(DrumTableModel).all() #queries the database, the SQL syntax is SELECT * from...
